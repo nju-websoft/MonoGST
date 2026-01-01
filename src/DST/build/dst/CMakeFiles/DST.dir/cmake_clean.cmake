a2332cec09ef66241f07097b9c6cd0f014ba848f
pinkyhead <1224430363@qq.com>
2026-01-01 15:20:03 +0800

    feat: 增加gitignore

--- src/DST/build/dst/CMakeFiles/DST.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libDST.a"
-  "libDST.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DST.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/DST/build/dst/CMakeFiles/DST.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DST.
-# This may be replaced when dependencies are built.
